897c1a36a97efaa3acb33db9045b62797d236a71
Arseniy Skrabneu <a.skrabneu@gmail.com>
2020-07-09 21:20:58 +0300

    Game menu added

--- Game.c
+++ Game.c
@@ -1,1 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 
+/*
+Turn-based strategy project
+---------------------------
+Started 9 June 2020
+By Anonymous
+---------------------------
+*/
+
+int main()
+{
+    //setting variables
+    char a;
+
+
+
+
+
+
+    //displaying start menu
+    printf("Menu:\n\n");
+    printf("1.New game\n");
+    printf("2.Load game\n");
+    printf("3.Settings\n");
+    printf("4.Help\n");
+    printf("5.Quit\n\n");
+
+    scanf("%c",&a);
+    printf("\n");
+    switch(a)
+    {
+        case '1':
+            {
+
+            }
+        break;
+
+        case '2':
+            {
+
+            }
+        break;
+
+        case '3':
+            {
+
+            }
+        break;
+
+        case '4':
+            {
+
+            }
+        break;
+
+        case '5':
+            {
+                goto q;
+            }
+        break;
+    }
+
+    q:
+        return 0;
+}
