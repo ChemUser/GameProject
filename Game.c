39a15c8aac956fdd0ec1b9dbc06eae1ff75e93c3
Arseniy Skrabneu <a.skrabneu@gmail.com>
2020-07-09 22:55:22 +0300

    Game field markup and display function added

--- Game.c
+++ Game.c
@@ -16,9 +16,40 @@ int main()
 {
     //setting variables
     char a;
+    char territory[79][49];
+    char resources[79][49];
+    char people[79][49];
 
+    int b;
 
+    //making game field markup
+    for(b=0;b<79;b++)
+    {
+        territory[b][0]=b;
+        resources[b][0]=b;
+        people[b][0]=b;
 
+        if(b<49)
+        {
+            territory[0][b]=b;
+            resources[0][b]=b;
+            people[0][b]=b;
+        }
+        if(b>0&&b<79)
+        {
+            territory[b][1]='-';
+            resources[b][1]='-';
+            people[b][1]='-';
+
+            if(b<49)
+            {
+                territory[1][b]='|';
+                resources[1][b]='|';
+                people[1][b]='|';
+            }
+        }
+    }
+    //end making game field markup
 
 
 
@@ -67,4 +98,17 @@ int main()
 
     q:
         return 0;
+}
+
+char show(char a[][49])
+{
+    int b,c;
+    for(b=0;b<49;b++)
+    {
+        for(c=0;c<79;c++)
+        {
+            printf("%c",a[c][b]);
+        }
+        printf("\n");
+    }
 }
